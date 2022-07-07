47b67642ac14464bbcc46581b4547733d4929115
Williams B <dbolade2000@gmail.com>
2022-07-07 16:03:04 +0100

    Icon added

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,21 +3,40 @@ import React, {useContext} from 'react';
 
 import {Context} from '../context/BlogContext';
 
+import Icon from 'react-native-vector-icons/Feather';
+
 export default function IndexScreen() {
   const {state, addBlogPost} = useContext(Context);
   return (
     <View>
-      <Text>IndexScreen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({item}) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Icon name="trash" style={styles.icon} />
+            </View>
+          );
         }}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+    color: '#000',
+  },
+});
